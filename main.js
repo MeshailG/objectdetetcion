8605f922dec56e428a26b1e7f61f795c6c3cae16
MeshailG <100999067+MeshailG@users.noreply.github.com>
2022-03-05 16:59:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+status="";
+img="";
+objects=[];
+function preload(){
+    img=loadImage('https://media.istockphoto.com/photos/happy-border-collie-dog-and-tabby-cat-together-closeup-picture-id1138523235?k=20&m=1138523235&s=612x612&w=0&h=K6lpiSJBvyqtghCESa9YsbKYrsvRJnS4Po0Jr8djuIw=');
+    
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.position(450,150);
+    objectdetector= ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelloaded(){
+    console.log("model is loaded !");
+    status=true;
+    objectdetector.detect(img, gotresults);
+}
+
+function gotresults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+function draw(){
+    image(img,0,0,640,420);
+   if(status!= "") {
+       for(index=0;index<objects.length;index+1){
+           document.getElementById("status").innerHTML= "Status : Object Detected";
+           fill(207, 132, 58);
+           percent=floor(objects[index]*100);   
+           text(objects[index].label + " " + percent + "%", objects[index].x,objects[index].y);
+           noFill();
+           stroke(147, 173, 99);
+           rect(objects[index].x, objects[index].y, objects[index].width , objects[index].height)       ;     
+            
+       }
+   }  
+
+    
+}
